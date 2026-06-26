c2537abdfba386e82b246eeed2adacd08f523022
= <=>
2026-06-27 01:38:41 +0600

    Code Update: required all packages and used all middleware as per needed

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const path = require("path");
+const {v4: uuidv4} = require("uuid");
+const mysql = require('mysql2');
+const methodOverride = require("method-override");
 const app = express();
 const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public/css")));
+app.use(express.static(path.join(__dirname, "/public/js")));
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.listen(port, () => {console.log(`Listening at port: ${port}`)});
